4f50e5819c20b7e6453b01128ecd8e8edeb80003
steff1234 <f1f1@live.dk>
2018-09-02 19:54:34 +0200

    Update Qbittorrent.sh

--- setup/Qbittorrent.sh
+++ setup/Qbittorrent.sh
@@ -3,15 +3,16 @@
 chmod +x ./Plex.sh
 
 a='sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y'
-b='sudo apt-get -y update 1>/dev/null 2>&1' 
-c='sudo apt-get -y install qbittorrent'
+b='sudo apt-get -y update' 
+c='sudo apt -y upgrade'
+d='sudo apt-get -y install qbittorrent'
 
 
 echo "Installer  Qbittorrent?"
 
 select answer in "Yes" "No"; do
     case $answer in
-        Yes ) echo "Installer Qbittorrent" && $a 1>/dev/null 2>&1;$b 1>/dev/null 2>&1;$c 1>/dev/null 2>&1; sudo ./Plex.sh;;
+        Yes ) echo "Installer Qbittorrent" && $a 1>/dev/null 2>&1;$b 1>/dev/null 2>&1;$c 1>/dev/null 2>&1;$d 1>/dev/null 2>&1; sudo ./Plex.sh;;
          No ) echo "NO" & sudo ./Plex.sh;;
           * ) echo "Please answer yes or no.";;
     esac
